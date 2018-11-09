eccd21ae2248cbac0f12eadb775b8f8c8437610f
andres <carlos.cuervo@correounivalle.edu.co>
2018-11-08 22:35:36 -0500

    updates

--- sporticket/sporticket/settings.py
+++ sporticket/sporticket/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'sport_db',
         'USER' : 'andres',
-        'PASSWORD': 'fuckencio1234',
+        'PASSWORD': '123456',
         'HOST' : 'localhost',
         'PORT' : 5432,
         
