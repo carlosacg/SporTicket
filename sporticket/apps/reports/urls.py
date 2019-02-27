e6e8491c934df537f997f93afe907d414a123fc9
andresow <andres.serrato@correounivalle.edu.co>
2019-02-27 15:09:04 -0500

    Report for events

--- sporticket/apps/reports/urls.py
+++ sporticket/apps/reports/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url, include
 from apps.reports.views import *
+from apps.reports.views import report_f
+
 
 urlpatterns = [
-	url(r'reports/reports.html', index_sale),
-	url(r'reports/reports.html', dasd, name='report1'),
+	#url(r'reports/reports.html', index_sale),
+	url(r'reports/reports.html', report_f, name='report1'),
 ]
--- sporticket/apps/reports/views.py
+++ sporticket/apps/reports/views.py
@@ -1,31 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from apps.reports.forms import ByEventsForms
+from apps.tickets.models import Ticket
 
-def index_sale(request):
-    if request.method == 'POST':
-        form = ByEventsForms(request.POST)
-        return redirect('home')
-    else:
-        form = ByEventsForms()  
-    return render(request, 'reports/reports.html',{'form': form})
     
-def dasd(request):
-    form = ByMovementReportForm()
-    selected_evebt = None
+def report_f(request, ):
+    print("entre")
+    form = ByEventsForms()
+    print("ey") 
     if request.method == 'POST':
         form = ByEventsForms(request.POST)
-        if form.is_valid():
-            selected_event = form.events
-            print(select_event)
-            return redirect('home')
-        else:
-            form = ByEventsForms()  
+        selected_event = form['events'].value()
+        print(selected_event)
+        avalibles =reportByEventAvalibles(int(selected_event))
+        sales = reportByEventSale(int(selected_event))
+        context = {'sales':sales,'avalibles':avalibles,'form':form}
+        return render(request, 'reports/reports.html',context)
+
+    else:
         return render(request, 'reports/reports.html',{'form': form})
 
-def reportByEvent(event_id):
-    ticket = Ticket.objects.all().filter(state='Disponible', event_id = event_id) 
-    return ticket.count()
 
-def reportByEvent(event_id):
-    ticket = Ticket.objects.all().filter(state='Vendido', event_id = event_id) 
-    return ticket.count()
+def reportByEventAvalibles(event_id):
+    print(event_id)
+    ticket = Ticket.objects.all().filter(state='Disponible', event_id = event_id).count()
+    print(ticket)
+    return ticket
+
+def reportByEventSale(event_id):
+    ticket = Ticket.objects.all().filter(state='Vendido', event_id = event_id).count()
+    return ticket
