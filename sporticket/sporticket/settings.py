78702cbb54fdec2963507c2928c081b983ef285c
carlosacg <carlos.cuervo@correounivalle.edu.co>
2019-03-22 21:09:53 -0500

    reload an messages shopping

--- sporticket/sporticket/settings.py
+++ sporticket/sporticket/settings.py
@@ -105,8 +105,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'sport_db',
-        'USER' : 'postgres',
-        'PASSWORD': '12345',
+        'USER' : 'andres',
+        'PASSWORD': '',
         'HOST' : 'localhost',
         'PORT' : 5432,    
     } 
